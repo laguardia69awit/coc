c8713b2b777dde825b5292fab655ddf0272471bd
laguardia69awit <111828535+laguardia69awit@users.noreply.github.com>
2022-09-05 21:28:05 +0800

    Create Character.java

---
+++ codes/Character.java
@@ -0,0 +1,28 @@
+public class Character {
+
+    // Character attributes
+
+    int strength;
+    int agility;
+    int intelligence;
+    String name;
+
+    // Character method
+
+public void sayMyName1() {
+    System.out.println("I am the leader " + name);
+}
+
+public void sayMyName2() {
+    System.out.println("I am the marksman " + name + " cutie");
+}
+
+public void sayMyName3() {
+    System.out.println("I am " + name + " the goblin");
+}
+
+public void sayMyName4() {
+    System.out.println("and last, I am " + name + " the support");
+}
+    
+}
