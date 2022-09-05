100664c07c969c3c3166b6aef8cbf382e1779eaf
laguardia69awit <111828535+laguardia69awit@users.noreply.github.com>
2022-09-05 21:28:40 +0800

    Create Enemy.java

---
+++ codes/Enemy.java
@@ -0,0 +1,47 @@
+public class Enemy {
+
+    // Enemy attributes
+
+    int damage;
+    int health;
+    int armor;
+    int speed;
+    boolean MagicImmunity;
+    String name;
+
+    // Enemy method
+
+public void enemyDialogueKing() {
+    System.out.println("WA! HA! HA! HA! I am the " + name + " I deal " + damage + " damage!");
+
+if (MagicImmunity = true) {
+    System.out.println("immunes to magic");
+}
+else {
+    System.out.println("unprotected to magic");
+}
+}
+
+public void enemyDialogueCthulhu() {
+    System.out.println("WA! HA! HA! HA! I am the " + name + ", I am very fast and my speed is " + speed );
+    
+if (MagicImmunity = true) {
+    System.out.println("immunes to magic");
+}
+else {
+    System.out.println("unprotected to magic");
+}
+}
+
+public void enemyDialogueDracarys() {
+    System.out.println("WA! HA! HA! HA! I am " + name + " I am very strong and I deal " + damage + " damage!");
+
+if  (MagicImmunity = true) {
+    System.out.println("immunes to magic");
+}
+else {
+    System.out.println("unprotected to magic");
+}
+}
+
+}
